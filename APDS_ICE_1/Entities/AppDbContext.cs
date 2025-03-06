98978c8ae8d5470d4baba265a46d76e1bce05921
User Name <mckylesingh71@gmail.com>
2025-03-06 12:06:55 +0200

    WIP

---
+++ APDS_ICE_1/Entities/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APDS_ICE_1.Entities
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Accounts>accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- APDS_ICE_1/Program.cs
+++ APDS_ICE_1/Program.cs
@@ -1,3 +1,7 @@
+using APDS_ICE_1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace APDS_ICE_1
 {
     public class Program
@@ -5,6 +9,8 @@ namespace APDS_ICE_1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
